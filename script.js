e5240e572eaa0689eeb7d6acfff6b48fdf3f77c5
2295084 <2295084@cegepoutaouais.qc.ca>
2023-01-16 12:03:13 -0500

    on a travaillé sur le script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+function Ajouter(){
+    let item = document.getElementById("desc").value;
+    let prix = document.getElementById("valeur").value; // Ajouter prix
+    let nouveauP = document.createElement("p");         // Créer une balise <p>
+    let nouveauSpan = document.createElement("span");
+    nouveauP.innerText = item+ " : ";
+    nouveauSpan.innerText = prix + "$";
+    document.getElementById("maList").append(nouveauP);   // Ajouter le paragraphe à la liste
+    nouveauP.append(nouveauSpan);                                   // mettre le span à la fin du p
+
+    // Mettre à jour le total
+    let ancienPrix = document.getElementById("total").innerText;
+    let nouveauPrix = +ancienPrix + +prix;                          // +variable : convertir en numérique
+    document.getElementById("total").innerText = nouveauPrix;
+}
+
 console.log("Bonjour");
